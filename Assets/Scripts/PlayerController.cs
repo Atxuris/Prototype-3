e9e73d43ff4c463ec4e5a88485d5e74890fc061f
Atxuris <getatx@gmail.com>
2026-07-03 17:57:02 -0300

    chore: forzar recalculado de lenguaje

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Variables
+    private Rigidbody playerRb;
+    public InputAction jumpAction;
+    public float jumpForce = 10f;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        jumpAction.Enable();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jumpAction.triggered && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+        
+    }
+
+    // test
+
+
+}
